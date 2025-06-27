ac35274ec9ef0773499e57faebb4d8a916467cb9
ACash01 <125855363+ACash01@users.noreply.github.com>
2025-06-27 18:44:46 +0800

    development parity, index 80% done

--- includes/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="flex flex-col justify-center align-center gap">
-    <h6>ZWhirl</h6>
-    <h6>8904 1137</h6>
-    <h6>aakash.raj.rai1027@gmail.com</h6>
-</footer>
--- includes/nav.php
+++
@@ -1,5 +0,0 @@
-<nav class="flex justify-evenly align-center gap-2">
-    <a class="flex align-center justify-center w-full h-full nav-left" href="./works.php">My Works</a>
-    <img src="./imgs/Logo-Main.svg" alt="My Logo">
-    <a class="flex align-center justify-center w-full h-full nav-right" href="./about.php">About Me</a>
-</nav>
